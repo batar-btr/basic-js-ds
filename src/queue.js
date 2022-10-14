8075b1a3a36f395e072de1898d319a95aaf9ae2f
unknown <btrn86@gmail.com>
2022-10-14 22:33:58 +0300

    feat: add stack and queue solutions

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+const last = list => {
+  let node = list;
+  while (node.next) {
+    node = node.next;
+  }
+  return node;
+};
+
+class Queue {
+  constructor() {
+    this.list = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.list;
+  }
+
+  enqueue(value) {
+    if(this.list) {
+      last(this.list).next = new ListNode(value);
+    } else {
+      this.list = new ListNode(value);
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let elem = this.list?.value;
+    this.list = this.list?.next;
+    return elem;
   }
 }
 
